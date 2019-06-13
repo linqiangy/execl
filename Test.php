47bb162c1ab2b0144f99e9a687037fa18bb5a579
linqiangy <tayloryang@kuainiugroup.com>
2019-06-13 14:27:34 +0800

    update

---
+++ Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linqiangy
+ * Date: 2019/6/13
+ * Time: 下午2:23
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World');
+
+$writer = new \PhpOffice\PhpSpreadsheet\Writer\Xls($spreadsheet);
+$writer->save('./hello.xls');
